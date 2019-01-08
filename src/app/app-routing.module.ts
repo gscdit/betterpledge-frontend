346a275acd2c7ad70e95d832e872155cd00505fb
BHUPESH <vipul.chikara.6002@gmail.com>
2019-01-09 02:09:49 +0530

    add static myorder page and fix some issue in cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MyProfileComponent } from "./Profile/my-profile/my-profile.component";
 import { AuthGuard } from "./auth.guard";
 import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
 import { CheckOutComponent } from "./check-out/check-out.component";
+import { MyOrderComponent } from "./Profile/my-order/my-order.component";
 
 const appRoutes: Routes = [
 { path:'',redirectTo:'/',pathMatch:'full'},
 {path:'Profile',component:MyProfileComponent,canActivate:[AuthGuard]},
 {path:'Cart',component:ShoppingCartComponent},
-{path:'check-out',component:CheckOutComponent}
+{path:'check-out',component:CheckOutComponent},
+{path:'order',component:MyOrderComponent}
 ];
 @NgModule({
     imports: [
