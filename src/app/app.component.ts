d974fea6bee12d0302b86c75056a419835e4d0b3
BHUPESH <vipul.chikara.6002@gmail.com>
2019-01-26 23:16:57 +0530

    ui fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'BetterPledge';
   onActivate(event) {
-    let scrollToTop = window.setInterval(() => {
-        let pos = window.pageYOffset;
-        if (pos > 0) {
-            window.scrollTo(0, pos - 20); // how far to scroll on each step
-        } else {
-            window.clearInterval(scrollToTop);
-        }
-    }, 16);
+   window.scroll(0,0);    
 }
 }
